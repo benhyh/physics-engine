217701b773cbeaf39795964e141c5483786fbc54
1 <bhyh.003@gmail.com>
2025-05-12 22:44:23 -0700

    Force

--- src/classes/Force.ts
+++ src/classes/Force.ts
@@ -2,17 +2,35 @@
  * Base class for all forces in the physics engine.
  * Each force type must implement the apply method.
  */
+
+import { RigidBody } from "./RigidBody";
+
+export enum ForceType {
+    GRAVITY,
+    SPRING,
+    DAMPING,
+    FRICTION,
+    AIR_RESISTANCE,
+    BUOYANCY
+}
+
 export abstract class Force {
     protected magnitude: number;
     protected direction: Vector;
     protected type: ForceType;
 
-    constructor(type: ForceType) {
+    constructor(
+        type: ForceType,
+        magnitude: number = 0,
+        direction: Vector = new Vector(0,0)
+    ) {
         // TODO: Initialize force properties
         // - Set force type
         // - Initialize magnitude and direction
+        this.type = type;
+        this.magnitude = magnitude;
+        this.direction = direction;
     }
-
     /**
      * Apply the force to a rigid body
      * @param body The rigid body to apply the force to
@@ -20,4 +38,16 @@ export abstract class Force {
      * Post-condition: force is added to body's force accumulator
      */
     abstract apply(body: RigidBody): void;
+
+    getType(): ForceType {
+        return this.type;
+    }
+
+    getMagnitude(): number {
+        return this.magnitude;
+    }
+
+    getDirection(): Vector {
+        return this.direction;
+    }
 }
