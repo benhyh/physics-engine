48b5d63f3c94c80e7a25a6fec3ee923e0bbff9eb
1 <bhyh.003@gmail.com>
2025-05-14 19:39:10 -0700

    Damping

--- src/classes/forces/basic/DampingForce.ts
+++ src/classes/forces/basic/DampingForce.ts
@@ -1,3 +1,34 @@
-import { Force } from "../base/Force";
+/**
+ * F = -cv where c is the damping coefficeint (in kg/s), v is the velocity vector
+ */
 
-export class DampingForce extends Force {}
+import { RigidBody } from "../../RigidBody";
+import { Force, ForceType } from "../base/Force";
+
+export class DampingForce extends Force {
+    private dampingCoefficient: number;
+    
+    constructor(dampingCoefficient: number = 1){
+        super(ForceType.DAMPING);
+        this.dampingCoefficient = dampingCoefficient;
+    }
+
+    apply(body: RigidBody): void {
+        // TODO:
+        // 1. get the current velocity of the body
+        // 2. calculates damping force using F = -cv
+        // 3. applies this force to the body
+        const velocity = body.velocity;
+        const force = velocity.multiply(this.dampingCoefficient * -1);
+        body.addForce(force);
+    }
+
+    
+    setDampingCoefficient(dampingCoefficient: number): void {
+        this.dampingCoefficient = dampingCoefficient;
+    }
+
+    getDampingCoefficient(): number {
+        return this.dampingCoefficient;
+    }
+}
